522f01910a8fc4eb29494b82355f1e972f66f2fc
Pierre Lafitte <lingtalfi@gmail.com>
2024-01-31 22:16:19 +0100

    first commit

---
+++ responsive-hooks.js
@@ -0,0 +1,61 @@
+(function(window) {
+  const screenBreakpoints = {
+    xs: 0,
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
+    xxl: 1400,
+  };
+
+
+
+
+  let currentBreakpoint = null;
+  const callbacks = {};
+
+  function getBreakpoint(width) {
+    let breakpoint = null;
+    for (let size in screenBreakpoints) {
+      if (width >= screenBreakpoints[size]) {
+        breakpoint = size;
+      }
+    }
+    return breakpoint;
+  }
+
+  function triggerCallbacks(breakpoint) {
+    if (callbacks[breakpoint]) {
+      callbacks[breakpoint].forEach(callback => callback());
+    }
+  }
+
+  function resizeHandler() {
+    const iw = window.innerWidth;
+    const newBreakpoint = getBreakpoint(iw);
+
+    if (newBreakpoint !== currentBreakpoint) {
+      currentBreakpoint = newBreakpoint;
+      triggerCallbacks(newBreakpoint);
+    }
+  }
+
+  window.ResponsiveHooks = {
+    on: function(breakpoint, callback) {
+      if (!callbacks[breakpoint]) {
+        callbacks[breakpoint] = [];
+      }
+      callbacks[breakpoint].push(callback);
+      if (currentBreakpoint === null) {
+        currentBreakpoint = getBreakpoint(window.innerWidth);
+      }
+
+      if (currentBreakpoint === breakpoint) {
+        callback();
+      }
+    }
+  };
+
+  window.addEventListener('resize', resizeHandler);
+  resizeHandler();
+})(window);
